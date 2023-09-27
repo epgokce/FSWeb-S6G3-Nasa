0661d586ad4d73490833b1ca6e71feb13eada80c
epgokce <ep.gokce@gmail.com>
2023-09-27 20:35:38 +0300

    nasa1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import React from "react";
 import "./App.css";
+import axios from "axios";
+import Gorsel from "./component/Gorsel";
 
 function App() {
+    const [nasa, setNasa] = useState({});
+  const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod", {
+        params: {
+          api_key: "c2859f48-935a-4fcf-a268-fa9f5291079c",
+          date: date,
+        },
+      })
+      .then(function (response) {
+        setNasa(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {});
+  }, [date]);
+
+  const dateSetter = (evt) => {
+    setDate(evt.target.value);
+  };
+
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <input type="date" id="date" name="date" onChange={dateSetter} />
+      <span role="img" aria-label="go!">
+        {!nasa.title && <p>Yükleniyor...</p>}
+      </span>
+      {nasa.title && <Gorsel data={nasa} />}
     </div>
   );
 }
